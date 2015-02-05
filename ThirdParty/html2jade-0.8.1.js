33f133c379f0bcbba843e2e143f9398674d9efad
Pascal Garber <jumplink@googlemail.com>
2015-02-05 19:12:42 +0000

    get example code from own about modal§

--- ThirdParty/html2jade-0.8.1.js
+++ ThirdParty/html2jade-0.8.1.js
@@ -4,11 +4,11 @@
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-    var isNode = false; 
+  var isNode = false; 
 
-    if(typeof module !== 'undefined' && module.exports) {
-      isNode = true;
-    };
+  if(typeof module !== 'undefined' && module.exports) {
+    isNode = true;
+  };
 
   if(isNode) {
     FS = require("fs");
